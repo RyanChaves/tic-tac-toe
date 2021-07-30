d818025ae9c1d309bdda4b9a22bc6a6142378774
rchav <angela@email.com>
2021-07-30 16:28:05 -0400

    updating colors and tally

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from tkinter import *
-YELLOW = "#f7f5dd"
+YELLOW = "#DAF7A6"
 FONT_NAME = "Bree Serif"
 GREEN ="#58C14F"
 BLUE= "#4FA7C1"
+DARK_BLUE= "#295E6E"
 
 
 #x starts the game
@@ -11,7 +12,8 @@ is_winner = False
 winner_symbol = ""
 #if count = 9 and no winner we have a tie
 count=0
-
+x_tally=0
+y_tally=0
 
 
 def btn_clicked(btn):
@@ -102,16 +104,22 @@ def check_winner():
         end_game()
 
 def end_game():
+    global x_tally, y_tally
     x_player_turn_label.grid_forget()
     o_player_turn_label.grid_forget()
     if winner_symbol == 'X':
         x_winner_label.grid(row=2, column=0)
+        x_tally+=1
+        x_tally_label.config(text=f"Wins\n {x_tally}")
     elif winner_symbol== 'O':
+        y_tally+=1
         y_winner_label.grid(row=2, column=4)
-    else:
-        tie_label.grid(row=5, column=3)
+        y_tally_label.config(text=f"Wins\n {y_tally}")
 
-    reset_btn.grid(row=5, column=1, columnspan=2)
+    else:
+        tie_label.grid(row=4, column=2, columnspan=2)
+
+    reset_btn.grid(row=4, column=2)
     disable_btns()
     print(f"{winner_symbol} is winner")
 
@@ -135,23 +143,23 @@ def reset_game():
     y_winner_label.grid_forget()
     tie_label.grid_forget()
     btn1 = Button(root, text="", command=lambda: btn_clicked(btn1), font=(FONT_NAME, 20, "bold"), height=2, width=5,
-                  bg=BLUE)
+                  bg=BLUE, activebackground=DARK_BLUE)
     btn2 = Button(root, text="", command=lambda: btn_clicked(btn2), font=(FONT_NAME, 20, "bold"), height=2, width=5,
-                  bg=BLUE)
+                  bg=BLUE, activebackground=DARK_BLUE)
     btn3 = Button(root, text="", command=lambda: btn_clicked(btn3), font=(FONT_NAME, 20, "bold"), height=2, width=5,
-                  bg=BLUE)
+                  bg=BLUE, activebackground=DARK_BLUE)
     btn4 = Button(root, text="", command=lambda: btn_clicked(btn4), font=(FONT_NAME, 20, "bold"), height=2, width=5,
-                  bg=BLUE)
+                  bg=BLUE, activebackground=DARK_BLUE)
     btn5 = Button(root, text="", command=lambda: btn_clicked(btn5), font=(FONT_NAME, 20, "bold"), height=2, width=5,
-                  bg=BLUE)
+                  bg=BLUE, activebackground=DARK_BLUE)
     btn6 = Button(root, text="", command=lambda: btn_clicked(btn6), font=(FONT_NAME, 20, "bold"), height=2, width=5,
-                  bg=BLUE)
+                  bg=BLUE, activebackground=DARK_BLUE)
     btn7 = Button(root, text="", command=lambda: btn_clicked(btn7), font=(FONT_NAME, 20, "bold"), height=2, width=5,
-                  bg=BLUE)
+                  bg=BLUE, activebackground=DARK_BLUE)
     btn8 = Button(root, text="", command=lambda: btn_clicked(btn8), font=(FONT_NAME, 20, "bold"), height=2, width=5,
-                  bg=BLUE)
+                  bg=BLUE, activebackground=DARK_BLUE)
     btn9 = Button(root, text="", command=lambda: btn_clicked(btn9), font=(FONT_NAME, 20, "bold"), height=2, width=5,
-                  bg=BLUE)
+                  bg=BLUE, activebackground=DARK_BLUE)
 
     btn1.grid(row=1, column=1)
     btn2.grid(row=1, column=2)
@@ -180,14 +188,19 @@ o_player_turn_label = Label(text='Your turn', bg=YELLOW, font=(FONT_NAME, 15, "b
 o_player_turn_label.grid(row=2, column=4)
 o_player_turn_label.grid_forget()
 
-bottom_label = Label(text='Tic-Tac-Toe', bg=YELLOW, font=(FONT_NAME, 20, "bold"))
-bottom_label.grid(row=4,column=1, columnspan=3)
+top_label = Label(text='Tic-Tac-Toe', bg=YELLOW, font=(FONT_NAME, 20, "bold"))
+top_label.grid(row=0,column=1, columnspan=3)
 
 x_winner_label = Label(text='Winner!', bg=YELLOW, font=(FONT_NAME, 20, "bold"))
 y_winner_label = Label(text='Winner', bg=YELLOW, font=(FONT_NAME, 20, "bold"))
 tie_label = Label(text='Tie!', bg=YELLOW, font=(FONT_NAME, 20, "bold"))
 
-reset_btn = Button(root,text='Restart?',font=(FONT_NAME, 20, "bold"), command=reset_game)
+reset_btn = Button(root,text='Restart?',font=(FONT_NAME, 13, "bold"),bg=BLUE, command=reset_game)
+
+x_tally_label = Label(text="Wins\n ", bg=YELLOW, font=(FONT_NAME, 20, "bold"))
+x_tally_label.grid(row=4, column=0)
+y_tally_label = Label(text="Wins\n ", bg=YELLOW, font=(FONT_NAME, 20, "bold"))
+y_tally_label.grid(row=4, column=4)
 
 reset_game()
 
